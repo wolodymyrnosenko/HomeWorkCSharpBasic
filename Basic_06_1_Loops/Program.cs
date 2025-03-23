96c216e7ffa627cd6cec4d111cdec8225b894987
Volodymyr Nosenko <vovanose@ukr.net>
2025-03-23 23:29:14 +0200

    Add 06_1 start menu

---
+++ Basic_06_1_Loops/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace Basic_06_1_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            string[] menuItems = {"0 - Завершення програми",
+                                "1 - Обчислення середнього заробітку" };
+            int userChoice = -1;
+            do
+            {
+                Console.Clear();
+                foreach(var menuItem in menuItems)
+                {
+                    Console.WriteLine(menuItem);
+                }
+                Console.Write("Введіть номер за списком: ");
+                try
+                {
+                    userChoice = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Введіть цифру");
+                    Console.ReadLine();
+                    continue;
+                }
+            }while(userChoice != 0);
+        }
+    }
+}
