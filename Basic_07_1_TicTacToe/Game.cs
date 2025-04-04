b912f410de924007ac8845bb951574fd69128367
volodymyr <vovanose@ukr.net>
2025-04-04 14:31:15 +0300

    Add field and function show

---
+++ Basic_07_1_TicTacToe/Game.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Basic_07_1_TicTacToe
+{
+    internal class Game
+    {
+        char[] cells = new char[9];
+        public void Start()
+        {
+            Show(cells);
+        }
+        void Show(char[] ar)
+        {
+
+            for (int i = 0; i < ar.Length; i++)
+            {
+                ar[i] = (char)(i + 49);
+                Console.Write($" {ar[i]} ");
+                if((i + 1) == 9)
+                {
+                    break;
+                }
+                if ((i+1)%3==0)
+                {
+                    Console.Write($"{Environment.NewLine}-----------{Environment.NewLine}");
+                    continue;
+                }
+                Console.Write("|");
+            }
+            Console.WriteLine($"{Environment.NewLine}{Environment.NewLine}хід ігрока: ");
+        }
+    }
+}
---
+++ Basic_07_1_TicTacToe/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace Basic_07_1_TicTacToe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Game game = new Game();
+            game.Start();
+
+        }
+    }
+}
