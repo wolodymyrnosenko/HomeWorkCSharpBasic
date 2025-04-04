a1f434e668b21618374c76cafc999c7fa9b868e0
volodymyr <vovanose@ukr.net>
2025-04-04 17:57:20 +0300

    Modified input

--- Basic_07_1_TicTacToe/Game.cs
+++ Basic_07_1_TicTacToe/Game.cs
@@ -1,25 +1,50 @@
 ﻿
 namespace Basic_07_1_TicTacToe
 {
+    enum player
+    {
+        O=79,
+        X=88
+    }
     internal class Game
     {
         char[] cells = new char[9];
         bool playerXtrueOfalse = true;
         //playerXtrueOfalse = !playerXtrueOfalse;
-        char move;
+        int move=0;//X~88, O~79
 
         public void Start()
         {
+            for (int i = 0; i < cells.Length; i++)
+            {
+                cells[i] = (char)(i + 49);
+            }
             Show(cells);
-            move = Convert.ToChar(Console.ReadKey());
-            Console.WriteLine();
+
+
+            //move = Convert.ToChar(Console.Read());
+            //if(move>=49 && move<=57)
+            //{
+            //    Console.WriteLine(cells[move-49]);
+            //    cells[move - 49] = (char)player.X;
+            //}
+
+            //move = int.Parse(Console.ReadKey());
+            //if(move>=1 && move<=9)
+            //{
+            //    Console.WriteLine(move);
+            //}
+
+
+            Show(cells);
+
         }
         void Show(char[] ar)
         {
             Console.Clear();
             for (int i = 0; i < ar.Length; i++)
             {
-                ar[i] = (char)(i + 49);
+                //ar[i] = (char)(i + 49);
                 Console.Write($" {ar[i]} ");
                 if((i + 1) == 9)
                 {
@@ -35,10 +60,10 @@ namespace Basic_07_1_TicTacToe
             Console.Write($"{Environment.NewLine}{Environment.NewLine}хід ігрока");
             if(playerXtrueOfalse)
             {
-                Console.WriteLine(" X: ");
+                Console.Write(" X: ");
             }else
             {
-                Console.WriteLine(" O: ");
+                Console.Write(" O: ");
             }
         }
     }
