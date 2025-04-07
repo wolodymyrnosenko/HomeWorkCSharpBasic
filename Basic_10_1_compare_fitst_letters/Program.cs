357fd92602a075736e4f58477d8d9ba04b75b145
Volodymyr Nosenko <vovanose@ukr.net>
2025-04-07 23:12:26 +0300

    Add task 10.1 and 10.3 string

---
+++ Basic_10_1_compare_fitst_letters/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace Basic_10_1_compare_fitst_letters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.Write("Введіть ім'я та прізвище: ");
+            string complexName = Console.ReadLine();
+            CompareFirstLetters(complexName);
+        }
+        public static void CompareFirstLetters(string txt)
+        {
+            int index = txt.IndexOf(" ");
+            Console.WriteLine(index);
+            var test = txt.Split(" ");
+        }
+    }
+}
---
+++ Basic_10_3_delete_spaces_string/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace Basic_10_3_delete_spaces_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            Console.Write("Введіть текст: ");
+            string text = Console.ReadLine();
+            Console.WriteLine($"Введений текст без пробілів: {DeleteSpacesInText(text)}");
+
+        }
+        public static String DeleteSpacesInText(string txt)
+        {
+            return txt.Replace(" ", "");
+        }
+    }
+}
