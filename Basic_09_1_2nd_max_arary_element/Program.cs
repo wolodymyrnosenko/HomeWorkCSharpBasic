49c85aa8b637e2499da1ce7531e34f1761793e80
volodymyr <vovanose@ukr.net>
2025-04-09 18:04:53 +0300

    Add task 9_1

---
+++ Basic_09_1_2nd_max_arary_element/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+
+namespace Basic_09_1_Arrays_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            string[] menuItems = {"0 - Завершення програми",
+                                "1 - Другий найбільший елемент у масиві",
+                                "2 - ",
+                                "3 - ",
+                                "4 - "};
+            int userChoice = -1;
+            do
+            {
+                Console.Clear();
+                foreach (var menuItem in menuItems)
+                {
+                    Console.WriteLine(menuItem);
+                }
+                Console.Write("Введіть номер за списком: ");
+                try
+                {
+                    userChoice = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Введіть цифру");
+                    Console.ReadLine();
+                    continue;
+                }
+                switch (userChoice)
+                {
+                    case 1:
+                        SecondMaxAraryElement.SecondMaxAraryElementFunction(7, 9);
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+
+                }
+            } while (userChoice != 0);
+        }
+    }
+}
---
+++ Basic_09_1_2nd_max_arary_element/SecondMaxAraryElement.cs
@@ -0,0 +1,96 @@
+﻿
+using System.Drawing;
+
+namespace Basic_09_1_Arrays_2
+{
+    internal class SecondMaxAraryElement
+    {
+        public static void SecondMaxAraryElementFunction(int rows, int columns)
+        {
+            int[][] array = new int[rows][];
+            for (int i = 0; i < rows; i++)
+            {
+                array[i] = new int[columns];
+            }
+            Random rand = new Random();
+            int[][] maxValueCoordinates = new int[2][];
+            maxValueCoordinates[0][] = new int[2];//To save max element
+            maxValueCoordinates[1][] = new int[2];//To save second max element
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    array[i][j] = rand.Next(minRand, maxRand + 1);
+                    Console.Write($"{array[i][j]}\t");
+
+                    if (i == 0 && j == 0)
+                    {
+                        max = array[i][j];
+                        countMax = 1;
+                        maxCoordinates[countMax * 2 - 2] = i;
+                        maxCoordinates[countMax * 2 - 1] = j;
+                        min = array[i][j];
+                        countMin = 1;
+                        minCoordinates[countMin * 2 - 2] = i;
+                        minCoordinates[countMin * 2 - 1] = j;
+                    }
+                    //max = (max > array[i][j] ? max : array[i][j]);
+                    //min = (min < array[i][j] ? min : array[i][j]);
+
+                    //Check is max and record coordinates
+                    if (max < array[i][j])
+                    {
+                        max = array[i][j];
+                        countMax = 1;
+                        maxCoordinates[countMax * 2 - 2] = i;
+                        maxCoordinates[countMax * 2 - 1] = j;
+                    }
+                    else if (max == array[i][j] && (i > 0 || j > 0))
+                    {
+                        countMax++;
+                        maxCoordinates[countMax * 2 - 2] = i;
+                        maxCoordinates[countMax * 2 - 1] = j;
+                    }
+
+                    //Check is min and record coordinates
+                    if (min > array[i][j])
+                    {
+                        min = array[i][j];
+                        countMin = 1;
+                        minCoordinates[countMin * 2 - 2] = i;
+                        minCoordinates[countMin * 2 - 1] = j;
+                    }
+                    else if (min == array[i][j] && (i > 0 || j > 0))
+                    {
+                        countMin++;
+                        minCoordinates[countMin * 2 - 2] = i;
+                        minCoordinates[countMin * 2 - 1] = j;
+                    }
+                }
+                Console.WriteLine();
+            }
+            if (max == min)
+            {
+                Console.WriteLine("Масив заповнений однаковими числами");
+            }
+
+
+
+
+            if (array.Length == 0)
+            {
+                return 0;
+            }
+            int max = array[0];
+            foreach (int ar in array)
+            {
+                if (max > ar)
+                {
+                    max = ar;
+                }
+            }
+            return max;
+
+        }
+    }
+}
