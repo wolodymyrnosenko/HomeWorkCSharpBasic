86b9b08235a8d6db2cff21f5696c73560bf556bd
volodymyr <vovanose@ukr.net>
2025-04-17 16:53:18 +0300

    Add task 11_1 Gallows (Hangman)

---
+++ Basic_11_1_Gallows/GuessWord.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Basic_11_1_Gallows
+{
+    public static class GuessWord
+    {
+        public static string RandomWord()
+        {
+            List<string> words = new List<string>
+            {
+                "FirstWord",
+                "ШИБЕНИЦЯ"
+            };
+            string strLorem = "Lorem Ipsum - це текст-\"риба\", що використовується в друкарстві та дизайні. Lorem Ipsum є, фактично, стандартною \"рибою\" аж з XVI сторіччя, коли невідомий друкар взяв шрифтову гранку та склав на ній підбірку зразків шрифтів. \"Риба\" не тільки успішно пережила п'ять століть, але й прижилася в електронному верстуванні, залишаючись по суті незмінною. Вона популяризувалась в 60-их роках минулого сторіччя завдяки виданню зразків шрифтів Letraset, які містили уривки з Lorem Ipsum, і вдруге - нещодавно завдяки програмам комп'ютерного верстування на кшталт Aldus Pagemaker, які використовували різні версії Lorem Ipsum.";
+            char[] separators = new char[]{ ' ', ',', '.', ';', ':', '-', '\"', '!', '?' };
+            string[] wordsFromLorem = strLorem.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < wordsFromLorem.Length; i++)
+            {
+                if (wordsFromLorem[i].Length > 3)
+                {
+                    words.Add(wordsFromLorem[i]);
+                }
+            }
+            return words[new Random().Next(words.Count)]; 
+        }
+    }
+}
---
+++ Basic_11_1_Gallows/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Reflection.Metadata;
+using System.Text;
+
+namespace Basic_11_1_Gallows
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            string guessWord = GuessWord.RandomWord().ToLower();
+            int countAttempt = guessWord.Length;
+            string userWord = new string('*', guessWord.Length);
+            string hello = "Вітаємо! Спробуйте вгадати зашифроване слово!";
+            Console.WriteLine(hello);
+
+            //Console.WriteLine(guessWord.PadLeft(hello.Length, ' '));//For testing GUESSED WORD
+
+            Console.WriteLine($"Кількість літер у слові: {guessWord.Length}");
+            Console.WriteLine($"Кількість можливих невірних спроб: {countAttempt}");
+            do
+            {
+                Console.WriteLine(userWord.PadLeft(hello.Length, ' '));
+                Console.Write("Введіть вашу літеру: ");
+                char inputSymbol = Console.ReadKey().KeyChar;
+                if(guessWord.Contains(inputSymbol))
+                {
+                    Console.WriteLine($"{Environment.NewLine}Така літера є у слові! Позиція літери: {LetterIndex(inputSymbol, guessWord, ref userWord)}");
+                }
+                else
+                {
+                    Console.WriteLine($"{Environment.NewLine}Такої літери немає! Залишилось спроб: {--countAttempt}");
+
+                }
+            } while (!isGameover(countAttempt, guessWord, userWord));
+            Console.WriteLine("Дякуємо за гру.");
+        }
+        public static StringBuilder LetterIndex(char inputSymbol, string guessWord, ref string userWord)
+        {
+            char[] tempWord = userWord.ToCharArray();//Create array to use in loop, cause userWord[i] only read
+            StringBuilder str = new StringBuilder();
+            string firstWithoutPoint = string.Empty;
+            for (int i = 0; i < guessWord.Length; i++)
+            {
+                if (guessWord[i] == inputSymbol)
+                {
+                    str = str.Append(firstWithoutPoint + (i + 1));
+                    firstWithoutPoint = ", ";
+                    tempWord[i] = guessWord[i];
+                }
+            }
+            userWord = new string(tempWord);//Change ref_userWord to output in console
+            return str;
+        }
+        static bool isGameover(int count, string word, string usWord)
+        {
+            if (word.Equals(usWord))
+            {
+                Console.WriteLine($"Вітаємо, ви вгадали слово! Зашифроване слово: {word}.");
+            }
+            return count == 0 || word.Equals(usWord);
+        }
+    }
+}
