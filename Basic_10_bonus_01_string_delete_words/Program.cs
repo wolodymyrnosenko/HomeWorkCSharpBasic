c5ef918ed52b894fa25c802afb65e4920cac5f5e
volodymyr <vovanose@ukr.net>
2025-04-18 13:01:14 +0300

    Add 10_1 bonus string delete words

---
+++ Basic_10_bonus_01_string_delete_words/Program.cs
@@ -0,0 +1,53 @@
+﻿//Створіть консольну програму на C#, яка обробляє великий текстовий рядок.
+//Вам потрібно видалити всі слова, які мають менше ніж 3 символи, і вивести результат на екран.
+//При цьому використовуйте StringBuilder для оптимізації роботи з рядками.
+//Опис:
+//Введіть великий текстовий рядок з консолі.
+//Розділіть рядок на окремі слова за допомогою пробілів або інших розділових знаків.
+//Видаліть всі слова, які мають менше ніж 3 символи.
+//Виведіть залишені слова на екран.
+using System.Text;
+
+namespace Basic_10_bonus_01_string_delete_words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Введіть великий текстовий рядок:");
+            string inputText = Console.ReadLine();
+
+            string result = TextProcessor.FilterWords(inputText);
+
+            Console.WriteLine("Результат:");
+            Console.WriteLine(result);
+        }
+    }
+
+    public class TextProcessor
+    {
+        public static string FilterWords(string inputText)
+        {
+            try
+            {
+                // код писати тут
+                char[] separators = new char[] { ' ', ',', '.', ';', ':', '-', '\"', '\\', '!', '?' };
+                string[] str = inputText.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                string text = string.Empty;
+                foreach (var st in str)
+                {
+                    if(st.Length >= 3)
+                    {
+                        text += $"{st} ";
+                    }
+                }
+                return text;
+            }
+            catch (Exception ex)
+            {
+                return "Помилка: " + ex.Message;
+            }
+        }
+    }
+}
