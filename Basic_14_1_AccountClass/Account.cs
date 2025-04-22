68ffd99eeab5e466f3daa168ef7dec4a4ecb9976
volodymyr <vovanose@ukr.net>
2025-04-22 16:56:28 +0300

    Add task 14_1 class ACCOUNT

---
+++ Basic_14_1_AccountClass/Account.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_14_1_AccountClass
+{
+    class Account
+    {
+        public string Name { get; }
+        private double balance;
+        public double Balance
+        {
+            get
+            {
+                return balance;
+            }
+            set
+            {
+                if(value >= 0)
+                {
+                    balance = value;
+                }
+                else
+                {
+                    Console.WriteLine("Стан рахунку не може бути менше за 0");
+                }
+            }
+        }
+        public Account(string name)
+        {
+            Name = name;
+            balance = 0.00;
+        }
+        public Account(string name, double balance)//public Account(string name, double balance = 0.00)//Такий варіант використовується? (щоб обійтись без конструктора з одним параметром)
+        {
+            Name = name;
+            Balance = balance;
+        }
+        public void Deposit(double sum)
+        {
+            Balance += sum;
+        }
+        public void Withdrawal(double sum)
+        {
+            Balance -= sum;
+        }
+    }
+}
---
+++ Basic_14_1_AccountClass/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Basic_14_1_AccountClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account heikkisAccount = new Account("Heikki's account", 100.00);
+            Account heikkisSwissAccount = new Account("Heikki's account in Switzerland", 1000000.00);
+            heikkisAccount.Withdrawal(20);
+            Console.WriteLine("The balance of Heikki's account is now: " + heikkisAccount.Balance);
+            heikkisSwissAccount.Deposit(200);
+            Console.WriteLine("The balance of Heikki's other account is now: " + heikkisSwissAccount.Balance);
+        }
+    }
+}
