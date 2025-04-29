d194a588a09b2705c8a96c61047b00b9b0849476
volodymyr <vovanose@ukr.net>
2025-04-29 14:14:53 +0300

    Add task 16_1 Copy txt file

---
+++ Basic_16_1_CopyTxtFile/FileCopier.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_16_1_CopyTxtFile
+{
+    public sealed class FileCopier
+    {
+        public string SourcePath { get; private set; }
+        public string DestinationPath { get; private set; }
+        private int bufferSize = 1024;//To copy by chank
+
+        public FileCopier(string sourcePath, string destinationPath)
+        {
+            SourcePath = sourcePath;
+            DestinationPath = destinationPath;
+        }
+
+        public void Copy()
+        {
+            if(!File.Exists(SourcePath))
+            {
+                Console.WriteLine("файл відсутній");
+                return;
+            }
+            if(!Directory.Exists(Path.GetDirectoryName(DestinationPath)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(DestinationPath));
+            }
+            byte[] buffer = new byte[bufferSize];
+            using (FileStream sourceStream = new FileStream(SourcePath, FileMode.Open, FileAccess.Read))
+            using (FileStream destinationStream = new FileStream(DestinationPath, FileMode.Create, FileAccess.Write))
+            {
+                int bytesRead;
+                while((bytesRead = sourceStream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    destinationStream.Write(buffer, 0, bytesRead);
+                }
+            }
+                Console.WriteLine("Файл успішно скопійовано!");
+        }
+    }
+}
---
+++ Basic_16_1_CopyTxtFile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace Basic_16_1_CopyTxtFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.Write("Введіть шлях до вихідного файлу: ");
+            string sourcePath = Console.ReadLine();
+            Console.Write("Введіть шлях до файлу, в якій потрібно скопіювати дані: ");
+            string destinationPath = Console.ReadLine();
+            try
+            {
+                new FileCopier(sourcePath, destinationPath).Copy();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Помилка: {ex.Message}");
+            }
+        }
+    }
+}
